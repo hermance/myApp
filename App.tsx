2637e732f7bfcc8fb055900377b7147cb64a701c
Hermance Dornier <hermancedornier@gmail.com>
2020-01-27 15:17:05 +0100

    eject

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>TEST</Text>
+      <Text>Open up App.tsx to start working on your app!</Text>
     </View>
   );
 }
